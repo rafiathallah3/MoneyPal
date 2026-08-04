2580a8cbf92280b64158c347700b9f3707fbed48
Rafi Athallah <65345768+rafiathallah3@users.noreply.github.com>
2026-08-05 00:56:29 +0700

    feat: update version bundle

--- app/preferences.tsx
+++ app/preferences.tsx
@@ -16,7 +16,7 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import i18n from '../utils/i18n';
 import HeaderAplikasi from './components/HeaderAplikasi';
 const APP_NAME = 'MoneyPal';
-const APP_VERSION = 'v1.1.3';
+const APP_VERSION = 'v1.1.4';
 
 const bahasaTeknologi = Localization.getLocales()[0]?.languageCode || 'en';
 
